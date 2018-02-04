85490e0a4489989a926ff5ecfe4d2f0af72db5c2
msuzuki <led.centipede@gmail.com>
2018-02-04 11:29:45 +0900

    baseクラス機能強化

--- TCMS-util.js
+++ TCMS-util.js
@@ -1,10 +1,83 @@
 javascript:(function(){
 	function TCMSUtil() {
-		d = document;
-		url = location.href;
-		selection = window.getSelection().toString();
-		uploadpath = "/file/img/";
-		rootpath = "https://www.pref.tokushima.lg.jp";
+		this.d = document;
+		this.url = location.href;
+		this.selection = window.getSelection().toString();
+		this.uploadpath = "/file/img/";
+		this.rootpath = "https://www.pref.tokushima.lg.jp";
+
+		this.menuWrap = this.d.getElementById("actions-sidebar").getElementsByClassName("side-nav").item(0);
+		this.menuList = this.menuWrap.getElementsByTagName("li");
+		this.menu_sitemap = this.menuList.item(0).getElementsByTagName("a").item(0);
+		this.menu_page = this.menuList.item(1).getElementsByTagName("a").item(0);
+		this.menu_new_page = this.menuList.item(2).getElementsByTagName("a").item(0);
+		this.menu_frame = this.menuList.item(3).getElementsByTagName("a").item(0);
+		this.menu_new_frame = this.menuList.item(4).getElementsByTagName("a").item(0);
+		this.menu_template = this.menuList.item(5).getElementsByTagName("a").item(0);
+		this.menu_new_template = this.menuList.item(6).getElementsByTagName("a").item(0);
+		this.menu_parts = this.menuList.item(7).getElementsByTagName("a").item(0);
+		this.menu_new_parts = this.menuList.item(8).getElementsByTagName("a").item(0);
+		this.menu_top = this.menuList.item(9).getElementsByTagName("a").item(0);
+		this.menu_new_top = this.menuList.item(10).getElementsByTagName("a").item(0);
+		this.menu_auth = this.menuList.item(11).getElementsByTagName("a").item(0);
+		this.menu_contact = this.menuList.item(12).getElementsByTagName("a").item(0);
+		this.menu_magazine = this.menuList.item(13).getElementsByTagName("a").item(0);
+		this.menu_settings = this.menuList.item(14).getElementsByTagName("a").item(0);
+
+		this.siteHash = {
+			"S11": "29",
+			"S21": "30",
+			"S22": "31",
+			"S31": "32",
+			"S32": "33",
+			"S33": "34",
+			"S34": "35",
+			"S41": "36",
+			"S42": "37",
+			"S43": "38",
+			"S44": "39",
+			"S45": "40",
+			"S51": "41",
+			"S52": "42",
+			"S61": "43",
+			"S62": "44",
+			"S71": "45",
+		};
+
+		this.siteSelect = null;
+		try { this.siteSelect = this.d.getElementById("site-groups"); } catch(e) {}
+
+		this.siteForm = null;
+		this.search_textbox = null;
+		try { this.siteForm =this.d.getElementsByClassName("search").item(0); } catch(e) {}
+		try { this.search_textbox = this.siteForm.name; } catch(e) {}
+
+		this.action_btns = null;
+		try { this.action_btns = this.d.getElementsByTagName("button"); } catch(e) {}
+
+		this.pageEditNav = null;
+		this.edit_btn = null;
+		this.basic_set_btn = null;
+		this.article_set_btn = null;
+		this.layout_set_btn = null;
+		this.pagedesign_set_btn = null;
+
+		try { this.pageEditNav = this.d.getElementsByClassName("line").item(0); } catch(e) {}
+		try { this.edit_btn = this.pageEditNav.getElementsByTagName("li").item(0).getElementsByTagName("a").item(0); } catch(e) {}
+		try { this.basic_set_btn = this.pageEditNav.getElementsByTagName("a").item(0); } catch(e) {}
+		try { this.article_set_btn = this.pageEditNav.getElementsByTagName("a").item(1); } catch(e) {}
+		try { this.layout_set_btn = this.pageEditNav.getElementsByTagName("a").item(2); } catch(e) {}
+		try { this.pagedesign_set_btn = this.pageEditNav.getElementsByTagName("a").item(3); } catch(e) {}
+
+		this.commit_btn = null;
+		try { this.commit_btn = this.d.getElementsByClassName("set-base btn").item(0); } catch(e) {}
+
+		this.save_btn = null;
+		try { this.save_btn = this.d.getElementsByClassName("draft-save btn").item(0); } catch(e) {}
+
+		this.auth_reset_btn = null;
+		try { this.auth_reset_btn = this.d.getElementById("approval-request"); } catch(e) {}
+
 
 	}
 	TCMSUtil.prototype = {
@@ -24,7 +97,7 @@ javascript:(function(){
 		upload_img_list_tsv: function(type) {
 			var str = "";
 			var rfpat = new RegExp(/(\()(.+?)(\))/);
-			var src = d.getElementById("upload-image-list");
+			var src = this.d.getElementById("upload-image-list");
 			var trs = src.rows;
 			for(var i=0; i<trs.length; i++) {
 				var tr = trs.item(i);
@@ -44,15 +117,15 @@ javascript:(function(){
 		upload_img_path: function(type) {
 			var rfpat = new RegExp(/(\()(.+?)(\))/);
 			var str = "";
-			if(typeof selection === "undefined" || selection === null) return;
-			if(!rfpat.test(selection)) return;
-			str = selection.match(rfpat)[2];
-			if(type === "popup") alert(uploadpath + str);
-			else this.browse_new_tab(uploadpath + str);
+			if(typeof this.selection === "undefined" || this.selection === null) return;
+			if(!rfpat.test(this.selection)) return;
+			str = this.selection.match(rfpat)[2];
+			if(type === "popup") alert(this.uploadpath + str);
+			else this.browse_new_tab(this.uploadpath + str);
 		},
 		get_tree_text: function(line_end) {
 			var str = "";
-			var ul = d.getElementsByClassName("list").item(0);
+			var ul = this.d.getElementsByClassName("list").item(0);
 			var ls = ul.getElementsByTagName("li");
 			for(var i=0; i<ls.length; i++) {
 				var li = ls.item(i);
@@ -64,12 +137,12 @@ javascript:(function(){
 		},
 		get_tree_text_link: function(sep, line_end) {
 			var str = "";
-			var ul = d.getElementsByClassName("list").item(0);
+			var ul = this.d.getElementsByClassName("list").item(0);
 			var ls = ul.getElementsByTagName("li");
 			for(var i=0; i<ls.length; i++) {
 				var li = ls.item(i);
 				var sts = li.getElementsByTagName("a").item(0);
-				var link = rootpath + sts.getAttribute("href");
+				var link = this.rootpath + sts.getAttribute("href");
 				var text = sts.textContent;
 				str += text + sep + link + line_end;
 			}
@@ -87,9 +160,9 @@ javascript:(function(){
 			else this.browse_new_tab(str)
 		},
 		draft_save: function() {
-			var btn0 = d.getElementsByClassName("decision-block btn").item(0);
-			var btn1 = d.getElementsByClassName("edit-end btn").item(0);
-			var btn2 = d.getElementsByClassName("draft-save btn").item(0);
+			var btn0 = this.d.getElementsByClassName("decision-block btn").item(0);
+			var btn1 = this.d.getElementsByClassName("edit-end btn").item(0);
+			var btn2 = this.d.getElementsByClassName("draft-save btn").item(0);
 			if(btn0 !== null) btn0.click();
 			if(btn1 !== null) btn1.click();
 			if(btn2 !== null) btn2.click();
@@ -97,12 +170,24 @@ javascript:(function(){
 		quick_edit_main_col: function(layout) {
 			var parent_class = "";
 			if(layout === "D") parent_class = "area area3";
-			var icn1 = d.getElementsByClassName(parent_class).item(0);
+			var icn1 = this.d.getElementsByClassName(parent_class).item(0);
 			if(icn1 !== null) icn1 = icn1.getElementsByClassName("edit-start click").item(0);
 			if(icn1 !== null) icn1.click();
-			var icn2 = d.getElementsByClassName("block-head").item(0);
+			var icn2 = this.d.getElementsByClassName("block-head").item(0);
 			if(icn2 !== null) icn2 = icn2.getElementsByClassName("edit-block click").item(0);
 			if(icn2 !== null) icn2.click();			
+
+		},
+		click_btn_by_keywd: function(str) {
+			var pt = new RegExp(".*" + str + ".*");
+			for(var i=0; i<this.action_btns.length; i++) {
+				var btn = this.action_btns.item(i);
+				var txt = btn.textContent;
+				if(pt.test(txt)) {
+					btn.click();
+					break;
+				}
+			}
 
 		}
 	};
